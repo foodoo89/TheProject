4243e0ff86ccee41053accf3ab8e2c10b5f06cf3
Michael White <michael.white308@gmail.com>
2015-04-22 19:40:39 +0800

    Added features
    Celsius to Fahrenheit conversion added. (Zyron)
    Miles to Kilometers added. (Zyron)

--- src/assignment1/Assignment1.java
+++ src/assignment1/Assignment1.java
@@ -16,12 +16,15 @@ import java.util.Scanner;
  * @author 041406846
  */
 public class Assignment1 {
-    final static double[] conversionRate = new double[3];                       // Array holding data for conversion rates
+    final static double[] conversionRate = new double[5];                       // Array holding data for conversion rates
     
     public static void main(String[] args){
         conversionRate[0] = 2.54;                                               // Variable for conversion cm to inch 
         conversionRate[1] = 0.3048;                                             // Variable for conversion m to feet
         conversionRate[2] = 0.453592;                                           // Variable for conversion pound to kg
+        conversionRate[3] = 33.8;
+        conversionRate[4] = 1.60;
+        
         int choice = 0;
         Scanner sc = new Scanner(System.in);                                    // initializing Scanner object to read input from keyboard
         do {                                                                    // looping for repeating the process
@@ -30,7 +33,9 @@ public class Assignment1 {
             System.out.println("1. Convert from cm to inch");
             System.out.println("2. Convert from m to feet");
             System.out.println("3. Convert from Pound to Kilogram");
-            System.out.println("4. Exit program");
+            System.out.println("4. Convert from Celsius to Farenheiht");
+            System.out.println("5. Convert from Miles to Kilometres");
+            System.out.println("6. Exit program");
             
             choice = sc.nextInt();                                              //read input from keyboard
             
@@ -55,12 +60,31 @@ public class Assignment1 {
                 System.out.println("The converted value " + "is "+round(output,2// display the result in kilograms
                         )+" kilograms.");
                 System.out.println("--------------------------------------");   // it is only seperator
-            } else if(choice == 4){
+            } 
+            else if(choice == 4)                                                //This is a code that will be processed when the user input is 4
+            {                                            
+                System.out.println("Input the value in celsius");               //This code will ask the user to insert the value of the celsius to be converted
+                double celsius = sc.nextDouble();                               //converting the user input into a double value
+                double output = celsius * conversionRate[3];                    //This is the equation for celsius into farenheiht
+                System.out.println("The converted value " + "is " + round(output,2)
+                        + " farenheiht. ");                                     //This code will display the answer to the user
+                System.out.println("--------------------------------------");   // it is only seperator
+            } 
+            else if(choice == 5)                                                //This is a code that will be processed when the user input is 5
+            {
+                System.out.println("Input the value in Miles");                 //This code will ask the user to insert the value of the miles to be converted
+                double miles = sc.nextDouble();                                 //Converting the user input into a double value
+                double output = miles * conversionRate[4];                      //This is the equation for miles into kilometres
+                System.out.println("The converted value " + "is " + round(output,2)
+                    + " kilometres");                                           //Displaying the answer
+                System.out.println("--------------------------------------");   // Seperator
+            }
+                else if(choice == 6){
                 System.out.println("Program terminates");       
                 System.exit(0);                                                 //to terminate whole process
             }  else {                                                           //this IF statement executes if input value is not 1, 2, 3, 4 or 5
             }
-        } while (choice != 4);                                                  // condition to run looping
+        } while (choice != 6);                                                  // condition to run looping
     }
     
     public static double round(double value, int places) { // for rounding into two digits after decimals
