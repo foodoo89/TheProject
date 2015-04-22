ee539901fdffecc460bce478daf0bd2fb9cebe69
Michael White <michael.white308@gmail.com>
2015-04-22 16:46:57 +0800

    Added pound to kilo conversion

--- src/assignment1/Assignment1.java
+++ src/assignment1/Assignment1.java
@@ -18,6 +18,7 @@ import java.util.Scanner;
 public class Assignment1 {
     final static double inci_const = 2.54;      // constant variable using for inci converter formula
     final static double feet_const = 0.3048;    // constant variable using for feet converter formula
+    final static double kilogram_const = 0.453592; // constant variable using for pound converter formula
     
     public static void main(String[] args){
         int choice = 0;
@@ -27,7 +28,8 @@ public class Assignment1 {
             System.out.println("Please select the option");
             System.out.println("1. Convert from cm to inch");
             System.out.println("2. Convert from m to feet");
-            System.out.println("3. Exit program");
+            System.out.println("3. Convert from Pound to Kilogram");
+            System.out.println("4. Exit program");
             
             choice = sc.nextInt();                                              //read input from keyboard
             
@@ -45,13 +47,19 @@ public class Assignment1 {
                 System.out.println("the converted value "                       // display the result in feet
                         + "is "+round(output,2)+" feet3");
                 System.out.println("--------------------------------------");   // it is only seperator
-            } else if(choice == 3){
+            } else if(choice == 3){                                             // this IF statement executes if input value is 3
+                System.out.println("Input the value in pounds");
+                double pound = sc.nextDouble();                                 // get the value of pound from the user
+                double output = pound * kilogram_const;                         // formula for converting pound to kg
+                System.out.println("The converted value " + "is "+round(output,2// display the result in kilograms
+                        )+" kilograms.");
+                System.out.println("--------------------------------------");   // it is only seperator
+            } else if(choice == 4){
                 System.out.println("Program terminates");       
                 System.exit(0);                                                 //to terminate whole process
-            }  else {                                                            //this IF statement executes if input value is not 1, 2 nor 3
-                System.out.println("invalid input!! Please Try Again!!");       //back to the looping head
+            }  else {                                                           //this IF statement executes if input value is not 1, 2, 3 or 4
             }
-        } while (choice != 3);                                                      // condition to run looping
+        } while (choice != 4);                                                  // condition to run looping
     }
     
     public static double round(double value, int places) { // for rounding into two digits after decimals
